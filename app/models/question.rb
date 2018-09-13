13f9eeb6297961defee9008d674d9091124ef4d8
DmitryKorobko <korobko.dmitry@gmail.com>
2018-09-13 12:37:59 +0300

    Remaked Attachable

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
   has_many :answers
-  has_many :attachments, as: :attachmentable
+  has_many :attachments, as: :attachable
 
   validates :title, :body, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_11_074228) do
+ActiveRecord::Schema.define(version: 2018_09_13_084202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 2018_09_11_074228) do
     t.string "file"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "attachmentable_id"
-    t.string "attachmentable_type"
-    t.index ["attachmentable_id", "attachmentable_type"], name: "index_attachments_on_attachmentable_id_and_attachmentable_type"
+    t.integer "attachable_id"
+    t.string "attachable_type"
+    t.index ["attachable_id", "attachable_type"], name: "index_attachments_on_attachable_id_and_attachable_type"
   end
 
   create_table "questions", force: :cascade do |t|
