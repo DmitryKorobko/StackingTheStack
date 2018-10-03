524166e62afcbe70495485aff6de58dd073d9704
DmitryKorobko <korobko.dmitry@gmail.com>
2018-10-03 14:10:31 +0300

    Advanced specs for some cases created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_01_110156) do
+ActiveRecord::Schema.define(version: 2018_10_03_092039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 2018_10_01_110156) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.boolean "admin", default: false
+    t.integer "reputation", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
